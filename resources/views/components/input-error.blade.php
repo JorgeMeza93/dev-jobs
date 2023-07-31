7a925268c7a8dda0b54c9fb70b9040173375b1f3
Jorge Meza Bello <jorberiver@gmail.com>
2023-07-31 17:15:56 -0600

    cambios en login interfaz

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,9 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1 mt-3']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+            <li class="bg-red-200 border-l-4 border-red-600 text-red-600 font-bold p-2">{{ $message }}</li>
         @endforeach
     </ul>
 @endif
